6d9cb2b155bb1ae422e4aad7c506bd769863d3a8
Maschell <Maschell@gmx.de>
2019-06-04 12:54:14 +0200

    Fix package declaration of NUSDataProviderWumad

--- src/de/mas/wiiu/jnus/implementations/NUSDataProviderWumad.java
+++ src/de/mas/wiiu/jnus/implementations/NUSDataProviderWumad.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
-package de.mas.wiiu.jnus;
+package de.mas.wiiu.jnus.implementations;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
